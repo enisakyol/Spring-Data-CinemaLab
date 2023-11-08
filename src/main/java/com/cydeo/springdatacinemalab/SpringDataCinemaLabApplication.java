59c0529b6f6929189390a98351cbeac3f9c08688
enis <enis_akyol@yahoo.com>
2023-11-08 17:41:02 +0300

    Cinemalab with mistakes

---
+++ src/main/java/com/cydeo/springdatacinemalab/SpringDataCinemaLabApplication.java
@@ -0,0 +1,22 @@
+package com.cydeo.springdatacinemalab;
+
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.output.MigrateResult;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class SpringDataCinemaLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataCinemaLabApplication.class, args);
+    }
+    @Bean
+    public MigrateResult migrateResult(DataSource dataSource){
+        return Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
+    }
+
+}
